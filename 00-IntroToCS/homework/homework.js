0612d3930584b10b2f25c0d6cf870a8f100a46c9
Fede <fedesapee182@outlook.com>
2022-04-25 17:01:02 -0300

    Homework 00

--- 00-IntroToCS/homework/homework.js
+++ 00-IntroToCS/homework/homework.js
@@ -2,15 +2,24 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+let suma = 0;
+let separado = num.split("").reverse();
+for(var i = 0; i < separado.length; i++){
+  let factor = 2**i;
+  suma += factor * separado[i];
+}
+return suma;
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+let suma = "";
+while(num !== 0){
+  suma = (num % 2) + suma; 
+  num = Math.floor( num / 2);
 }
-
-
+return suma;
+}
 module.exports = {
   BinarioADecimal,
   DecimalABinario,
